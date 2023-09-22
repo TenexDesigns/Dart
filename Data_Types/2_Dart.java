696d52c875b29d9d6589f6a742acad366bea6524
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-22 13:59:55 +0300

    Create 2_Dart.java

---
+++ Data_Types/2_Dart.java
@@ -0,0 +1,94 @@
+In Dart, like many other programming languages, you can work with various data types to store and manipulate different kinds of values. Here are some common data types in Dart:
+
+1. **Numbers:**
+   - `int`: Represents integers (whole numbers) like 1, 42, -10.
+   - `double`: Represents floating-point numbers (numbers with decimal points) like 3.14, -0.5.
+
+   Example:
+   ```dart
+   int age = 30;
+   double price = 12.99;
+   ```
+
+2. **Strings:**
+   - `String`: Represents sequences of characters, enclosed in single (' ') or double (" ") quotes.
+
+   Example:
+   ```dart
+   String name = 'John';
+   String greeting = "Hello, World!";
+   ```
+
+3. **Booleans:**
+   - `bool`: Represents a Boolean value, either `true` or `false`.
+
+   Example:
+   ```dart
+   bool isRaining = true;
+   bool isSunny = false;
+   ```
+
+4. **Lists:**
+   - `List`: Represents a collection of objects, which can be of different types, and is ordered and indexed. Lists can be defined using square brackets `[]`.
+
+   Example:
+   ```dart
+   List<int> numbers = [1, 2, 3, 4, 5];
+   List<String> fruits = ['apple', 'banana', 'cherry'];
+   ```
+
+5. **Maps:**
+   - `Map`: Represents a collection of key-value pairs, where keys are unique. Maps can be defined using curly braces `{}`.
+
+   Example:
+   ```dart
+   Map<String, int> scores = {
+     'Alice': 95,
+     'Bob': 88,
+     'Charlie': 72,
+   };
+   ```
+
+6. **Dynamic:**
+   - `dynamic`: Represents a type that can change at runtime. It can hold values of any data type.
+
+   Example:
+   ```dart
+   dynamic x = 42;
+   x = 'Hello, Dart!';
+   ```
+
+7. **Null:**
+   - `null`: Represents the absence of a value. Variables can be assigned the value `null`.
+
+   Example:
+   ```dart
+   String? nullableString = null;
+   ```
+
+8. **Object:**
+   - `Object`: The root type of the Dart type hierarchy. All types are subtype of `Object`.
+
+   Example:
+   ```dart
+   Object obj = 'Some value';
+   ```
+
+9. **Functions:**
+   - `Function`: Represents a function type, which can be used to define function variables.
+
+   Example:
+   ```dart
+   int add(int a, int b) {
+     return a + b;
+   }
+   ```
+
+These are the fundamental data types in Dart. You can create variables of these types to store and manipulate data in your Dart programs. Dart also supports type inference, which means you can omit the type annotation, and Dart will automatically determine the type based on the assigned value. For example:
+
+```dart
+var age = 30; // Dart infers that age is an int.
+var name = 'John'; // Dart infers that name is a String.
+```
+
+Dart provides a rich set of libraries and tools for working with these data types, making it a versatile language for various application types.
