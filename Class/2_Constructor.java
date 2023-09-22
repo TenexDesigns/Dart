10a155ec459e4152d8515980483897b62088012a
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-22 17:33:22 +0300

    Create 2_Constructor.java

---
+++ Class/2_Constructor.java
@@ -0,0 +1,70 @@
+In Dart, constructors are special methods used to create and initialize objects of a class. They allow you to set initial values for the instance variables (properties) of a class when an object is created. Dart provides several types of constructors, each with its own use case:
+
+1. **Default Constructor:**
+   - This is also known as the "no-argument constructor" or "empty constructor."
+   - If you don't define any constructors in your class, Dart automatically provides a default constructor with no arguments.
+   - It initializes instance variables to their default values (e.g., `null` for objects, `0` for integers).
+
+   ```dart
+   class Person {
+     String name;
+
+     // Default constructor provided by Dart
+     // No need to define it explicitly
+   }
+   ```
+
+2. **Parameterized Constructor:**
+   - A parameterized constructor allows you to initialize instance variables when creating an object.
+   - You can define multiple parameterized constructors in a class by providing different parameter lists.
+
+   ```dart
+   class Person {
+     String name;
+     int age;
+
+     // Parameterized constructor
+     Person(this.name, this.age);
+   }
+
+   void main() {
+     var person = Person('Alice', 30);
+   }
+   ```
+
+3. **Named Constructor:**
+   - Named constructors are additional constructors with specific names.
+   - They are used for creating objects with different initialization logic or for providing alternative ways to create objects.
+
+   ```dart
+   class Rectangle {
+     double width;
+     double height;
+
+     // Named constructor for creating a square
+     Rectangle.square(double side) : width = side, height = side;
+   }
+
+   void main() {
+     var square = Rectangle.square(5.0);
+   }
+   ```
+
+4. **Constant Constructor:**
+   - A constant constructor is used to create objects that are compile-time constants.
+   - All instance variables must be marked as `final` or `const` in a class that uses a constant constructor.
+
+   ```dart
+   class Circle {
+     final double radius;
+
+     // Constant constructor
+     const Circle(this.radius);
+   }
+
+   void main() {
+     const myCircle = Circle(3.0); // A compile-time constant
+   }
+   ```
+
+Constructors in Dart provide flexibility in how you create and initialize objects. You can use them to ensure that objects are properly initialized and ready to use when they are created. Depending on your application's requirements, you can choose the appropriate type of constructor for your classes.
