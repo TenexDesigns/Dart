568b4d066bddaa4f9ce4203c6965f0e8cc1977ef
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-09-22 21:17:08 +0300

    Create 1_Static .java

---
+++ Static/1_Static .java
@@ -0,0 +1,75 @@
+The `static` keyword in Dart is used to declare class-level variables and methods. Class-level variables are shared by all instances of the class, and class-level methods can be called without creating an instance of the class.
+
+**Static variables**
+
+Static variables are declared using the `static` keyword followed by the variable name. For example, the following code shows how to declare a static variable in a `Person` class:
+
+```dart
+class Person {
+  static int count = 0;
+
+  Person() {
+    count++;
+  }
+
+  static int getCount() {
+    return count;
+  }
+}
+```
+
+The `count` variable is a static variable because it is declared with the `static` keyword. This means that all instances of the `Person` class will share the same `count` variable.
+
+To access a static variable, you use the class name followed by the variable name. For example, the following code shows how to access the `count` variable:
+
+```dart
+int personCount = Person.count;
+```
+
+You can also call static methods on a class without creating an instance of the class. For example, the following code shows how to call the `getCount()` method on the `Person` class:
+
+```dart
+int personCount = Person.getCount();
+```
+
+**Static methods**
+
+Static methods are declared using the `static` keyword followed by the method name. For example, the following code shows how to declare a static method in a `Person` class:
+
+```dart
+class Person {
+  static int getCount() {
+    return count;
+  }
+}
+```
+
+The `getCount()` method is a static method because it is declared with the `static` keyword. This means that you can call the `getCount()` method without creating an instance of the `Person` class.
+
+To call a static method, you use the class name followed by the method name. For example, the following code shows how to call the `getCount()` method:
+
+```dart
+int personCount = Person.getCount();
+```
+
+**Uses of static variables and methods**
+
+Static variables and methods are useful for a variety of purposes, such as:
+
+* Storing global data that is shared by all instances of a class.
+* Implementing utility methods that can be used without creating an instance of a class.
+* Providing a way to access class-level data and methods without having to create an instance of the class.
+
+Here are some examples of how to use static variables and methods in Dart:
+
+* **Storing global data:** You could use a static variable to store the current user's logged-in status.
+* **Implementing utility methods:** You could use a static method to generate a random number.
+* **Providing a way to access class-level data and methods:** You could use a static method to get the total number of people who have created instances of a `Person` class.
+
+Static variables and methods are a powerful tool for writing reusable and efficient code. By understanding how to use static variables and methods in Dart, you can write more robust and reliable code.
+
+
+
+
+
+  
