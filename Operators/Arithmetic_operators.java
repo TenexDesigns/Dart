53331460130b8925630b57c96d72f10009d9f5a9
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-04 15:45:57 +0300

    Create Arithmetic_operators.java

---
+++ Operators/Arithmetic_operators.java
@@ -0,0 +1,75 @@
+Dart supports various arithmetic operators that allow you to perform mathematical operations on numbers. Here are the common arithmetic operators in Dart, along with code samples:
+
+1. **Addition (+):**
+   The `+` operator is used for addition.
+
+   ```dart
+   int a = 5;
+   int b = 3;
+   int sum = a + b;
+   print('Sum: $sum'); // Output: Sum: 8
+   ```
+
+2. **Subtraction (-):**
+   The `-` operator is used for subtraction.
+
+   ```dart
+   int a = 10;
+   int b = 7;
+   int difference = a - b;
+   print('Difference: $difference'); // Output: Difference: 3
+   ```
+
+3. **Multiplication (*):**
+   The `*` operator is used for multiplication.
+
+   ```dart
+   int a = 4;
+   int b = 6;
+   int product = a * b;
+   print('Product: $product'); // Output: Product: 24
+   ```
+
+4. **Division (/):**
+   The `/` operator is used for division. If both operands are integers, the result is a double.
+
+   ```dart
+   int dividend = 10;
+   int divisor = 3;
+   double result = dividend / divisor;
+   print('Result: $result'); // Output: Result: 3.3333333333333335
+   ```
+
+5. **Integer Division (~/):**
+   The `~/` operator performs integer division, returning only the integer part of the division result, discarding the remainder.
+
+   ```dart
+   int dividend = 10;
+   int divisor = 3;
+   int quotient = dividend ~/ divisor;
+   print('Quotient: $quotient'); // Output: Quotient: 3
+   ```
+
+6. **Modulus (%):**
+   The `%` operator calculates the remainder of the division.
+
+   ```dart
+   int dividend = 10;
+   int divisor = 3;
+   int remainder = dividend % divisor;
+   print('Remainder: $remainder'); // Output: Remainder: 1
+   ```
+
+7. **Increment (++) and Decrement (--):**
+   The `++` operator is used to increment a variable by 1, and the `--` operator is used to decrement a variable by 1.
+
+   ```dart
+   int count = 5;
+   count++; // Increment count by 1
+   print('Count: $count'); // Output: Count: 6
+
+   count--; // Decrement count by 1
+   print('Count: $count'); // Output: Count: 5
+   ```
+
+These are the basic arithmetic operators in Dart. You can use these operators to perform various mathematical calculations in your Dart programs.
